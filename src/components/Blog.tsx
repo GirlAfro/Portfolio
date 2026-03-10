e404177645584b0cce7f01baec37766a7f7d60fd
GirlAfro <kimchigarira13@gmail.com>
2026-03-10 18:32:09 +0200

    Create Blog component that displays blog posts

---
+++ src/components/Blog.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const blogPosts = [
+  {
+    title: 'Understanding React',
+    excerpt: 'A brief overview of React and its benefits.',
+    date: '2026-03-10',
+    category: 'React',
+    link: '#'
+  },
+  {
+    title: 'How to Use Hooks',
+    excerpt: 'An introduction to using hooks in React.',
+    date: '2026-03-09',
+    category: 'React',
+    link: '#'
+  },
+  {
+    title: 'JavaScript ES6 Features',
+    excerpt: 'Exploring the new features introduced in ES6.',
+    date: '2026-03-08',
+    category: 'JavaScript',
+    link: '#'
+  }
+];
+
+const Blog = () => {
+  return (
+    <div className="blog-section">
+      <h2>Blog Posts</h2>
+      <div className="blog-posts">
+        {blogPosts.map((post, index) => (
+          <div key={index} className="blog-post">
+            <h3>{post.title}</h3>
+            <p>{post.excerpt}</p>
+            <small>{post.date} | {post.category}</small>
+            <a href={post.link}>Read more</a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
